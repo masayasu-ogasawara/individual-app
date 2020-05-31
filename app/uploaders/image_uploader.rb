b3ccefb0aed5082aa7c53464f2dfaedf6ff7111e
masayasu-ogasawara <ogasawara.masayasu@gmail.com>
2020-05-31 18:52:48 +0900

    setting deploy

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -10,13 +10,13 @@ class ImageUploader < CarrierWave::Uploader::Base
     storage :fog
   end
   process resize_to_fit: [100, 100]
-  
+
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-
+  
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
